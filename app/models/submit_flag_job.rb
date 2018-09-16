3c3fc8226d9dbf0b5026a4f91dd184f82f32b47e
zackgilbert <zgilbert@foursquare.com>
2018-09-16 11:11:15 -0500

    Adjustments based on Yinon's comments.

--- app/models/submit_flag_job.rb
+++ app/models/submit_flag_job.rb
@@ -30,17 +30,16 @@ class SubmitFlagJob < Struct.new(:flag, :token)
 
       # Photo and Tip flags have their own endpoint with their own rate limit
       if ["PhotoFlag", "TipFlag"].include? flag.type
-        flags_of_type = flag.user.flags.where(:status => "queued").where(:type => flag.type).count
+        flags_of_type = flag.user.flags.where(:status => ["queued", "submitted"]).where(:type => flag.type).count
         rate_limit = 500
       else
-        flags_of_type = flag.user.flags.where(:status => "queued").where("type NOT IN (?)", ["PhotoFlag", "TipFlag"]).count
+        flags_of_type = flag.user.flags.where(:status => ["queued", "submitted"]).where("type NOT IN (?)", ["PhotoFlag", "TipFlag"]).count
         rate_limit = 5000
       end
       # If this fails due to rate_limit_exceeded, figure out how long it will take to process all flags
       # of this type and randomly assign a time in that window. It's hacky and brittle, but better than
       # every job retrying at the same time.
       # TODO: make this reschedule all likely to fail flags, not just this one
-      # TODO: create CheckFlagJob so that similar logic could be applied to that
       return time_now + (60*60 * ((flags_of_type / rate_limit) + 1) * rand).to_i
     else
       return time_now + (attempts ** 4) + 5 #default exponential backoff
